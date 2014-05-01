a99395cfcfd08781c8775c9d1a08ecbad3dbeeca
jannunzi <jannunzi@gmail.com>
2014-05-01 09:32:59 -0400

    Average with loop

--- Iteration/Average/Main.cpp
+++ Iteration/Average/Main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void main() {
 	float average = 0.0;
 	float total = 0.0;
-	int size = 10;
+	int size = 20;
 	for (int i = 0; i < size; i++) {
 		total += i;
 		cout << i << ") " << total << endl;
