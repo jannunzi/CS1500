f938154b95a29c18bbdc11834cd67d138f21b049
jannunzi <jannunzi@gmail.com>
2014-05-01 00:01:00 -0400

    Average with for loop

---
+++ Iteration/Average/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	float average = 0.0;
+	float total = 0.0;
+	int size = 10;
+	for (int i = 0; i < size; i++) {
+		total += i;
+		cout << i << ") " << total << endl;
+	}
+	cout << "total: " << total << endl;
+	cout << "size: " << size << endl;
+	average = total / size;
+	cout << "average: " << average << endl;
+	getchar();
+}
