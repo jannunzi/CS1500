5808dd6a908ebaf664bf913fedc488f587c50508
jannunzi <jannunzi@gmail.com>
2014-05-05 07:27:15 -0400

    Lab 1 Hello World Solution

---
+++ Labs/Lab1/Annunziato_Jose_Lab1_Hello_World.cpp
@@ -0,0 +1,8 @@
+#include <iostream>	// for cout and endl
+#include <conio.h>	// for getch() (optional)
+using namespace std;	// optional
+
+void main() {
+	cout << "Hello World!" << endl;
+	getch(); // optional
+}
