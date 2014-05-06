97e25c4df77521b5ff0f829dde8f0bbc07a00655
jannunzi <jannunzi@gmail.com>
2014-05-05 22:02:49 -0400

    renamed solution directory

--- Labs/Lab1/Annunziato_Jose_Lab1_Hello_World.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>	// for cout and endl
-#include <conio.h>	// for getch() (optional)
-using namespace std;	// optional
-
-void main() {
-	cout << "Hello World!" << endl;
-	getch(); // optional
-}
