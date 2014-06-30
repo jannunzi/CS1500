e11bf21dba70d519bd9b16b017ca3a2e2e807600
jannunzi <jannunzi@gmail.com>
2014-06-29 20:36:35 -0400

    Lab 1

---
+++ 2014/Summer2/Assignments/Lab1/Lab1_Hello_World_Annunziato_Jose.cpp
@@ -0,0 +1,9 @@
+#include <iostream>		// for cout and endl
+#include <conio.h>		// for getch() (optional)
+using namespace std;	// optional
+
+void main()
+{
+	cout << "Hello World!" << endl;
+	getch(); // optional
+}
