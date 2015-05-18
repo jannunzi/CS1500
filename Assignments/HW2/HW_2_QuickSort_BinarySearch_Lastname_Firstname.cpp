ff7d902ce63839290e118ee9bd2e46c5a3ac68f0
jannunzi <jannunzi@gmail.com>
2015-05-18 11:05:03 -0400

    Update HW_2_QuickSort_BinarySearch_Lastname_Firstname.cpp

--- Assignments/HW2/HW_2_QuickSort_BinarySearch_Lastname_Firstname.cpp
+++ Assignments/HW2/HW_2_QuickSort_BinarySearch_Lastname_Firstname.cpp
@@ -11,33 +11,33 @@ int  binarySearchForPrice(float searchPrice, int first, int last, float appPrice
 
 void main()
 {
-	string appNames[] =
-	{
-		"Pages", "Keynote", "Numbers",
-		"Word", "PowerPoint", "Excel",
-		"Documents", "Presentation", "Sheets"
-	};
+    string appNames[] =
+    {
+        "Pages", "Keynote", "Numbers",
+        "Word", "PowerPoint", "Excel",
+        "Documents", "Presentation", "Sheets"
+    };
 
-	float appPrices[] =
-	{
-		3.99, 2.99, 1.99,
-		19.99, 29.99, 39.99,
-		34.99, 24.99, 14.99
-	};
+    float appPrices[] =
+    {
+        3.99, 2.99, 1.99,
+        19.99, 29.99, 39.99,
+        34.99, 24.99, 14.99
+    };
 
-	displayAllApplications(appNames, appPrices, 9);
-	quickSort(0, 8, appNames, appPrices);
-	displayAllApplications(appNames, appPrices, 9);
-	int foundIndex = binarySearchForPrice(25.99, 0, 9, appPrices);
+    displayAllApplications(appNames, appPrices, 9);
+    quickSort(0, 8, appNames, appPrices);
+    displayAllApplications(appNames, appPrices, 9);
+    int foundIndex = binarySearchForPrice(25.99, 0, 9, appPrices);
 
-	if (foundIndex == -1)
-	{
-		cout << "Not Found" << endl;
-	}
-	else
-	{
-		cout << "Found at index " << foundIndex << endl;
-	}
+    if (foundIndex == -1)
+    {
+        cout << "Not Found" << endl;
+    }
+    else
+    {
+        cout << "Found at index " << foundIndex << endl;
+    }
 
-	getchar();
+    getchar();
 }
